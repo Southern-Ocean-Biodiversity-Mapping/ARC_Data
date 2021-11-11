8f232943edbf3955d07ba76e5672c8f09911c375
Nicole Hill <Nicole.Hill@utas.edu.au>
2021-11-11 12:44:05 +1100

    Update with BSOSE vars

--- prep_environment/2_Extract_Environment_at_Samples.R
+++ prep_environment/2_Extract_Environment_at_Samples.R
@@ -33,17 +33,18 @@ env_names<-gsub(".*_|\\..*","",env_list)
 env_stack<-stack(env_list)
 names(env_stack)
 names(env_stack)[1:5]<-env_names[1:5]
-names(env_stack)[6:14]<-paste(rep(c("CARS_NO3", "CARS_O2", "CARS_PO4"),each=3),c("mean", "seas_range", "std_dev"), sep="_")
-names(env_stack)[25]<-"NPP_su_mean"
-names(env_stack)[38:39]<-env_names[13:14]
+names(env_stack)[14:22]<-paste(rep(c("CARS_NO3", "CARS_O2", "CARS_PO4"),each=3),c("mean", "seas_range", "std_dev"), sep="_")
+names(env_stack)[33]<-"NPP_su_mean"
+names(env_stack)[46:47]<-env_names[17:18]
 
 
-#add environmental data with non-conformant names
+#add environmental data with non-conformant names- 
+#### remember to update column index if changes!!!
 env_stack<-stack( env_stack,
                   raster(paste0(env.derived, "Circumpolar_EnvData_geomorphology")))
-names(env_stack)[40]<-"geomorph"
+names(env_stack)[48]<-"geomorph"
 
-geomorph_cat<-levels(env_stack[[40]])[[1]]
+geomorph_cat<-levels(env_stack[[48]])[[1]]
 
 
 ## 3) Match environmental data to image data (at cell level) ----

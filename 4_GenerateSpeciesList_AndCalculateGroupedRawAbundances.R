0354193165b9179a2453bc84431d7d3ccc3b5daa
Jan Jansen <Jan.Jansen@utas.edu.au>
2022-12-07 10:45:16 +1100

    fixed some species naming and calculated image based statistics for Shaylyn

--- 4_GenerateSpeciesList_AndCalculateGroupedRawAbundances.R
+++ 4_GenerateSpeciesList_AndCalculateGroupedRawAbundances.R
@@ -54,17 +54,35 @@ cover_prev<-data.frame(count=colSums(cover_cells>0)) %>%
   add_column(Exclude= "",
              Merge_With= "",
              Other_Notes="")
+#... & at the image level:
+img.cover_prev<-data.frame(count=colSums(cover_images>0)) %>%
+  mutate(., prev= round(count/nrow(cover_images), 3 )) %>%
+  rownames_to_column(., var="Label") %>%
+  arrange(., desc(count))%>%
+  add_column(Exclude= "",
+             Merge_With= "",
+             Other_Notes="")
 
 ##2b) Percent cover
 # remove unscorable points
 cover_cells2<-cover_cells %>%
   dplyr::select(., -Unscorable) 
+#... & at the image level:
+cover_images2<-cover_images %>%
+  dplyr::select(., -Unscorable) 
 
 # percent cover of taxa in each cell
-cover_pc<-prop.table(as.matrix(cover_cells ), margin=1)*100 
-  
+cover_pc<-prop.table(as.matrix(cover_cells), margin=1)*100 
+#... & at the image level:
+img.cover_pc<-prop.table(as.matrix(cover_images), margin=1)*100  
+
 # proportion of all scored points assigned to each taxa
 cover_pc_overall<- data.frame(perc_overall=colSums(prop.table(as.matrix(cover_cells))*100)) %>%
+  rownames_to_column(., var="Taxa") %>%
+  arrange(., desc(perc_overall))%>%
+  mutate(perc_overall= round(perc_overall, 3)) 
+#... & at the image level:
+img.cover_pc_overall<- data.frame(perc_overall=colSums(prop.table(as.matrix(cover_images))*100)) %>%
   rownames_to_column(., var="Taxa") %>%
   arrange(., desc(perc_overall))%>%
   mutate(perc_overall= round(perc_overall, 3)) 
@@ -75,11 +93,15 @@ rownames_to_column(., var="Taxa") %>%
   arrange(., desc(perc_mean))%>%
   mutate(perc_mean= round(perc_mean, 3)) 
 #very similar to overall percent cover
+#... & at the image level:
+img.cover_pc_mean<- data.frame(perc_mean= apply(img.cover_pc, 2, mean))%>%
+  rownames_to_column(., var="Taxa") %>%
+  arrange(., desc(perc_mean))%>%
+  mutate(perc_mean= round(perc_mean, 3)) 
 
 
 ## 3) COUNTS ----
 ## 3a) Prevalence
-  
 count_prev<-data.frame(count=colSums(count_cells>0)) %>%
   mutate(., prev= round(count/nrow(count_cells), 3 )) %>%
   rownames_to_column(., var="Label") %>%
@@ -88,10 +110,22 @@ count_prev<-data.frame(count=colSums(count_cells>0)) %>%
   add_column(Exclude= "",
              Merge_With= "",
              Other_Notes="")
+#... & at the image level:
+img.count_prev<-data.frame(count=colSums(count_images>0)) %>%
+  mutate(., prev= round(count/nrow(count_images), 3 )) %>%
+  rownames_to_column(., var="Label") %>%
+  arrange(., desc(count)) %>%
+  #add extra columns for notes
+  add_column(Exclude= "",
+             Merge_With= "",
+             Other_Notes="")
 
 ## 3b) Overall abundance
-
 count_ab_overall<-data.frame(total_count=colSums(count_cells))%>%
+  rownames_to_column(., var="Taxa") %>%
+  arrange(., desc(total_count))
+#... & at the image level:
+img.count_ab_overall<-data.frame(total_count=colSums(count_images))%>%
   rownames_to_column(., var="Taxa") %>%
   arrange(., desc(total_count))
 
@@ -104,6 +138,12 @@ count_ab_overall<-data.frame(total_count=colSums(count_cells))%>%
 #           COUNT_totAb= count_ab_overall),
 #           path=paste0(ARC_Data.dir, "Annotation/Species_list_",res,"_2022_10.xlsx"))
 
+# write_xlsx(x= list(COVER_prevalence=img.cover_prev,
+#           COVER_overall=img.cover_pc_overall,
+#           COUNT_prevalence= img.count_prev,
+#           COUNT_totAb= img.count_ab_overall),
+#           path=paste0(ARC_Data.dir, "Annotation/Species_list_images_2022_12.xlsx"))
+
 
 #######################################################################
 
@@ -111,9 +151,11 @@ count_ab_overall<-data.frame(total_count=colSums(count_cells))%>%
 # blanks read in as NAs
 
 ## 5a) cover data
-mod_cover_list<-read_xlsx(path=paste0(ARC_Data.dir, "Annotation/Species_list_",res,"_2022_10.xlsx"),
-sheet=1)
+mod_cover_list<-read_xlsx(path=paste0(ARC_Data.dir, "Annotation/Species_list_",res,"_2022_12.xlsx"),sheet=1)
 names(mod_cover_list)[5:6]<- c("Merge_1pc", "Merge_2pc")
+
+img.mod_cover_list<-read_xlsx(path=paste0(ARC_Data.dir, "Annotation/Species_list_images_2022_12.xlsx"),sheet=1)
+names(img.mod_cover_list)[5]<- "Merge_2pc"
 
 
 #reformat to long, merge, change names and convert back to wide
@@ -122,10 +164,17 @@ cover_cells_long<-cover_cells %>%
   pivot_longer( cols=`1Sub_Fine`:Echinoderms_Crinoids_Stalked, 
                 names_to ="Label", values_to = "count") %>%           #long format and merge names to change
   left_join( mod_cover_list[,c("Label", "Merge_2pc")])
-
 cover_cells_long$new<-  ifelse(!is.na(cover_cells_long$Merge_2pc), cover_cells_long$Merge_2pc, cover_cells_long$Label)
-
 cover_cells_renamed<-pivot_wider(cover_cells_long, id_cols=cellID, names_from = new, values_from = count, values_fn=sum,values_fill = 0)
+
+cover_images_long<-cover_images %>%
+  mutate(cellID=rownames(cover_images))  %>% #add cellID
+  pivot_longer( cols=`1Sub_Fine`:Echinoderms_Crinoids_Stalked, 
+                names_to ="Label", values_to = "count") %>%           #long format and merge names to change
+  left_join(img.mod_cover_list[,c("Label", "Merge_2pc")])
+cover_images_long$new<-  ifelse(!is.na(cover_images_long$Merge_2pc), cover_images_long$Merge_2pc, cover_images_long$Label)
+cover_images_renamed<-pivot_wider(cover_images_long, id_cols=cellID, names_from = new, values_from = count, values_fn=sum,values_fill = 0)
+
 
 #remove species to exclude (DON'T REMOVE BECAUSE WE MESS UP TOTAL COVER DATA?)
 cover_mod<-cover_cells_renamed
@@ -133,11 +182,18 @@ cover_mod<-cover_cells_renamed
 #   select( - mod_cover_list$Label[which(mod_cover_list$Exclude =='x')])
 cover_mod$cellID<-as.factor(cover_mod$cellID)
 
+img.cover_mod<-cover_images_renamed
+# cover_mod<-cover_cells_renamed %>%
+#   select( - mod_cover_list$Label[which(mod_cover_list$Exclude =='x')])
+img.cover_mod$cellID<-as.factor(img.cover_mod$cellID)
+
 
 ## 5b) count data
-mod_count_list<-read_xlsx(path=paste0(ARC_Data.dir, "Annotation/Species_list_",res,"_2022_10.xlsx"),
-                          sheet=3)
+mod_count_list<-read_xlsx(path=paste0(ARC_Data.dir, "Annotation/Species_list_",res,"_2022_12.xlsx"),sheet=3)
 names(mod_count_list)[5:6]<- c("Merge_1pc", "Merge_2pc")
+
+img.mod_count_list<-read_xlsx(path=paste0(ARC_Data.dir, "Annotation/Species_list_images_2022_12.xlsx"),sheet=3)
+names(img.mod_count_list)[5]<- "Merge_2pc"
 
 #reformat to long, merge, change names and convert back to wide
 count_cells_long<-count_cells %>%
@@ -145,19 +201,28 @@ count_cells_long<-count_cells %>%
   pivot_longer( cols=Echinoderms__Crinoid_unstalked:Molluscs__Gastropods_shell__LimpetLike, 
                         names_to ="Label", values_to = "count") %>%           #long format and merge names to change
   left_join( mod_count_list[,c("Label", "Merge_2pc")])
-
 count_cells_long$new<-  ifelse(!is.na(count_cells_long$Merge_2pc), count_cells_long$Merge_2pc, count_cells_long$Label)
-
 count_cells_renamed<-pivot_wider(count_cells_long, id_cols=cellID, names_from = new, values_from = count, values_fn=sum,values_fill = 0)
+
+count_images_long<-count_images %>%
+  mutate(cellID=rownames(count_images))  %>% #add cellID
+  pivot_longer( cols=Echinoderms__Crinoid_unstalked:Molluscs__Gastropods_shell__LimpetLike, 
+                names_to ="Label", values_to = "count") %>%           #long format and merge names to change
+  left_join( mod_count_list[,c("Label", "Merge_2pc")])
+count_images_long$new<-  ifelse(!is.na(count_images_long$Merge_2pc), count_images_long$Merge_2pc, count_images_long$Label)
+count_images_renamed<-pivot_wider(count_images_long, id_cols=cellID, names_from = new, values_from = count, values_fn=sum,values_fill = 0)
 
 #remove species to exclude
 count_mod<-count_cells_renamed %>%
  dplyr::select( - mod_count_list$Label[which(mod_count_list$Exclude =='x')])
+img.count_mod<-count_images_renamed %>%
+  dplyr::select( - img.mod_count_list$Label[which(img.mod_count_list$Exclude =='x')])
 # #some of these categories no longer exist. Only need to exclude 'Tube"
 # count_mod <- count_cells_renamed %>%
 #   select(!Tube)
 
 count_mod$cellID<-as.factor(count_mod$cellID)
+img.count_mod$cellID<-as.factor(img.count_mod$cellID)
 
 ##### 
 
@@ -202,7 +267,7 @@ cover_SF <- rowSums(cover_cells_renamed2[,sel_SF])
 cover_SF_pa <- cover_SF
 cover_SF_pa[cover_SF>0] <- 1
 richness <- rowSums(cover_cells_pa[,-sel_sed])
-richness.l <- rowSums(cover_cells_pa[,-sel_sed])/log(cell_metadata_env$cover_points_total)
+richness.l <- rowSums(cover_cells_pa[,-sel_sed])/log(scorable.pts)#(cell_metadata_env$cover_points_total)
 cover_all.prop <- rowSums(cover_cells_renamed2[,-sel_sed])/scorable.pts
 cover_all <- rowSums(cover_cells_renamed2[,-sel_sed])
 
@@ -250,8 +315,87 @@ count_groupings <- data.frame(cbind(count_mobile, count_echino, count_crust, cou
 save(cover_mod, count_mod, cover_groupings, count_groupings, file=paste0(ARC_Data.dir,"Cell_level_bio_2pc_",res,".Rdata"))
 
 
+##### FOR IMAGES INSTEAD OF CELLS:
+## 6) Calculate abundances of functional groups and richness on the raw data (before species are excluded)
+# ## first read in cell-metadata
+load(paste0(ARC_Data.dir,"Cell_level_env_500m.Rdata"))
 
+## need to remove the cellID from the columns to properly calculate rowSums etc
+cover_images_renamed2 <- data.frame(cover_images_renamed[,-1])
+rownames(cover_images_renamed2) <- cover_images_renamed$cellID
 
+## 6a) cover data
+## names of faunal groups for cover_cells_renamed (minus cellID):
+dataset.names <- names(cover_images_renamed2)
+## selector for each faunal class
+sel_S <- grep("Sp",substr(dataset.names,1,2))
+sel_O <- grep("Oc",substr(dataset.names,1,2))
+sel_B <- grep("Bry",substr(dataset.names,1,3))
+sel_M <- grep("Mo",substr(dataset.names,1,2))
+sel_E <- grep("Ech",substr(dataset.names,1,3))
+sel_Asc <- grep("Asc",substr(dataset.names,1,3))
+sel_TW <- grep("Worms_Polychaetes_T",dataset.names)
+sel_Hy <- grep("Hyd",dataset.names)
+## selector for functional group
+sel_SF <- c(sel_S,sel_O,sel_B,sel_Asc,sel_TW,sel_Hy)
+## selector for sediment class etc
+sel_sed_soft <- c(grep("Fine",dataset.names),grep("PbGrv",dataset.names))
+sel_sed_loose <- c(grep("Cbble",dataset.names),grep("BioRu",dataset.names),grep("BioShl",dataset.names),grep("BioOth",dataset.names)) 
+sel_sed_hard <- c(grep("Bould",dataset.names),grep("Rock",dataset.names))
+sel_sed <- grep("Sub_",dataset.names)
+sel_noid.cov <- grep("NoID",dataset.names)
+sel_unsc.cov <- grep("Unscorable",dataset.names)
+
+## calculating species richness etc, keeping in mind that the first column is the cellID
+scorable.pts <- 108-cover_images_renamed2$Unscorable
+
+cover_images_pa <- cover_images_renamed2
+cover_images_pa[cover_images_pa>0] <- 1
+
+img.cover_SF.prop <- rowSums(cover_images_renamed2[,sel_SF])/scorable.pts
+img.cover_SF <- rowSums(cover_images_renamed2[,sel_SF])
+img.cover_SF_pa <- img.cover_SF
+img.cover_SF_pa[img.cover_SF>0] <- 1
+img.richness <- rowSums(cover_images_pa[,-sel_sed])
+img.richness.l <- rowSums(cover_images_pa[,-sel_sed])/log(scorable.pts)
+img.cover_all.prop <- rowSums(cover_images_renamed2[,-sel_sed])/scorable.pts
+img.cover_all <- rowSums(cover_images_renamed2[,-sel_sed])
+
+img.cover_B.prop <- rowSums(cover_images_renamed2[,sel_B])/scorable.pts
+img.cover_B <- rowSums(cover_images_renamed2[,sel_B])
+img.cover_B_pa <- img.cover_B
+img.cover_B_pa[img.cover_B>0] <- 1
+
+img.cover_S.prop <- rowSums(cover_images_renamed2[,sel_S])/scorable.pts
+img.cover_S <- rowSums(cover_images_renamed2[,sel_S])
+img.cover_S_pa <- img.cover_S
+img.cover_S_pa[img.cover_S>0] <- 1
+
+## large species groupings - cover
+img.cover_groupings <- data.frame(cbind(img.cover_all, img.cover_SF, img.cover_SF_pa, img.richness, img.richness.l, img.cover_B, img.cover_B_pa, img.cover_S, img.cover_S_pa))
+
+##### setup biological data - counts
+## names of faunal groups for count_images:
+count.names <- names(count_images)
+
+## selector for each faunal class
+sel_noid <- grep("NoID",count.names)
+sel_echino <- grep("Echinoderms",count.names)
+sel_crust <- grep("Crustacea",count.names)
+
+img.count_mobile <- rowSums(count_images[,-sel_noid]) ## remove NoIDs
+img.count_echino <- rowSums(count_images[,sel_echino])
+img.count_crust <- rowSums(count_images[,sel_crust])
+
+count_images_pa <- count_images
+count_images_pa[count_images_pa>0] <- 1
+img.count_richness <- rowSums(count_images_pa[,-sel_sed])
+
+## large species groupings - counts
+img.count_groupings <- data.frame(cbind(img.count_mobile, img.count_echino, img.count_crust, img.count_richness))
+
+# save outputs
+save(img.cover_mod, img.count_mod, img.cover_groupings, img.count_groupings, file=paste0(ARC_Data.dir,"Cell_level_bio_images.Rdata"))
 
 
 

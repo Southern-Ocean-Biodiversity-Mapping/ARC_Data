cf345907987163ff81d657f3afda5099d08d5533
Rex-Troll <xavieralvy@gmail.com>
2026-07-06 22:02:50 +1000

    Add Dropbox directory for user 'PC'

--- 0_SourceFile.R
+++ 0_SourceFile.R
@@ -13,6 +13,10 @@ if(usr == "JJ"){
   usr.dropbox.dir <- "C:/Users/jjansen/UTAS Research Dropbox/Jan Jansen/Data/"
 }
 
+if(usr == "PC"){
+  usr.dropbox.dir <- "C:\Users\xavie\Dropbox\data_environmental"
+}
+
 ## polar stereographic projection for Antarctica:
 #stereo <- "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
 stereo <- "EPSG:9354"
